a06bd5a0673c359d72f44e63bb8a2811c6adabc1
LEI-122695 <matildegit5@gmail.com>
2025-12-04 22:41:57 +0000

    Initial commit

---
+++ src/test/java/SamplerInteraction/SamplerInteraction.java
@@ -0,0 +1,11 @@
+package SamplerInteraction;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.ElementsCollection;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+// page_url = about:blank?wi_0
+public class SamplerInteraction {
+}
