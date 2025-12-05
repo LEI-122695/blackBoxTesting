4b93c0bf02416b5eae3308523b18067bc3f322cb
LEI-122695 <matildegit5@gmail.com>
2025-12-05 15:12:08 +0000

    Add implementation of SamplerInteraction and related tests

--- src/test/java/SamplerInteraction/SamplerInteraction.java
+++ src/test/java/SamplerInteraction/SamplerInteraction.java
@@ -1,11 +1,40 @@
 package SamplerInteraction;
 
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.ElementsCollection;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.visible;
 
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
-
-// page_url = about:blank?wi_0
 public class SamplerInteraction {
+
+    // URL do site oficial do Vaadin Sampler
+    private static final String URL = "https://demo.vaadin.com/sampler/";
+
+    // --- ELEMENTOS ---
+    private final SelenideElement userInterfaceMenu = $(byText("User interface"));
+    private final SelenideElement dataInputMenu = $(byText("Data input"));
+
+    // --- MÉTODOS (Ações) ---
+
+    public void open() {
+        Selenide.open(URL);
+
+        // CORREÇÃO CRÍTICA:
+        // Obrigamos o teste a esperar que o menu "User interface" apareça.
+        // Só depois disso é que o site é considerado "carregado" e o título estará disponível.
+        userInterfaceMenu.shouldBe(visible);
+    }
+
+    public void clickUserInterface() {
+        userInterfaceMenu.shouldBe(visible).click();
+    }
+
+    public void clickDataInput() {
+        dataInputMenu.shouldBe(visible).click();
+    }
+
+    public String getTitle() {
+        return Selenide.title();
+    }
 }
